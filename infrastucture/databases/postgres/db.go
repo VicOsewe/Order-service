1571aab96418926c62d461ebe5dcfc8dc630753e
Victorine Osewe <victorine.osewe@savannahinformatics.com>
2022-03-18 13:29:36 +0300

    chore:set up postgres

---
+++ infrastucture/databases/postgres/db.go
@@ -0,0 +1,51 @@
+package postgres
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func SetUpDB() error {
+
+	por := getEnv("DBPORT")
+	password := getEnv("DBPASSWORD")
+	user := getEnv("DBUSER")
+	dbname := getEnv("DBNAME")
+	host := getEnv("DBHOST")
+	port, err := strconv.Atoi(por)
+	if err != nil {
+		return fmt.Errorf("failed to get port with error: %v", err)
+	}
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return fmt.Errorf("failed to open database:%v", err)
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		return fmt.Errorf("failed to establish a connection to the database:%v", err)
+	}
+	return nil
+
+}
+
+func getEnv(envName string) string {
+	envVar := os.Getenv(envName)
+	if envVar == "" {
+		msg := fmt.Sprintf("environment variable %s not found", envName)
+		log.Panicf(msg)
+		os.Exit(1)
+	}
+
+	return envVar
+}
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/VicOsewe/Order-service/infrastucture/databases/postgres"
 	"github.com/gorilla/mux"
 )
 
@@ -12,6 +15,12 @@ func main() {
 
 func SetUpRouter() {
 	router := mux.NewRouter()
+	//connect to db
+	err := postgres.SetUpDB()
+	if err != nil {
+		log.Printf("failed to connect to database :%v", err)
+		os.Exit(1)
+	}
 
 	//start and listen to requests
 	http.ListenAndServe(":8080", router)
